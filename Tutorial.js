b06e6823f20043ec7ecc91fe24eca8e171af25c8
Joshua Ferge <josh.ferge@gmail.com>
2015-01-18 14:34:35 -0700

    push

--- Tutorial.js
+++ Tutorial.js
@@ -1299,7 +1299,15 @@ $(function () {
      });
 
 	$( "#RightPad" ).draggable({ 
-
+		scroll:true,
+   start: function(){
+      $(this).data("startingScrollTop",$(this).parent().scrollTop());
+   },
+   drag: function(event,ui){
+      var st = parseInt($(this).data("startingScrollTop"));
+      ui.position.top -= $(this).parent().scrollTop() - st;
+   }
+   
 		snap: "#RightTarget" , 
 		snapMode: "inner" ,
 		stop: function( event, ui ) {
