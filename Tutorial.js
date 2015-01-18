54178d0d34e9717a2f7d1e42e135ad82cea3e53c
Joshua Ferge <josh.ferge@gmail.com>
2015-01-18 14:35:42 -0700

    sdf

--- Tutorial.js
+++ Tutorial.js
@@ -1300,14 +1300,14 @@ $(function () {
 
 	$( "#RightPad" ).draggable({ 
 		scroll:true,
-   start: function(){
-      $(this).data("startingScrollTop",$(this).parent().scrollTop());
-   },
-   drag: function(event,ui){
-      var st = parseInt($(this).data("startingScrollTop"));
-      ui.position.top -= $(this).parent().scrollTop() - st;
-   }
-   
+	   start: function(){
+	      $(this).data("startingScrollTop",$(this).parent().scrollTop());
+	   },
+	   drag: function(event,ui){
+	      var st = parseInt($(this).data("startingScrollTop"));
+	      ui.position.top -= $(this).parent().scrollTop() - st;
+	   },
+
 		snap: "#RightTarget" , 
 		snapMode: "inner" ,
 		stop: function( event, ui ) {
